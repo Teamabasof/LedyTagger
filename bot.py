f19f8302b8e899a7188d8ec9e30da8fbe4f49dda
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-26 19:31:23 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def start(event):
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**🌹Ledy Tagger Bot'un Ərmləri və haqqında🌹**\n\Haqqında:\n/bu bot, qruplarda 10.000'ə qədər useri və kanallarda 200'ə qədər useri tağ edə bilər. nəzərinizə çatdırımki bot'u qrupda admin huquqları verin yoxsa bot cavab verməyəcək!\n\nƏmrlər:\n/all <səbəb> - 👀5-li tag edəcəkdir. \n/tagadmin <səbəb> - 👻Yalnız adminləri tag edəcəkdir. \n/tektag <səbəb> - 😁Tək-tək tag edəcəkdir.\n/etag <səbəb> - 😋Emojilər ilə tag edəcəkdir."
+  helptext = "**🌹Ledy Tagger Bot'un Ərmləri və haqqında🌹**\n\ Haqqında:\nbu bot, qruplarda 10.000'ə qədər useri və kanallarda 200'ə qədər useri tağ edə bilər. nəzərinizə çatdırımki bot'u qrupda admin huquqları verin yoxsa bot cavab verməyəcək!\n\nƏmrlər:\n/all <səbəb> - 👀5-li tag edəcəkdir. \n/tagadmin <səbəb> - 👻Yalnız adminləri tag edəcəkdir. \n/tektag <səbəb> - 😁Tək-tək tag edəcəkdir.\n/etag <səbəb> - 😋Emojilər ilə tag edəcəkdir."
   await event.reply(helptext,
                     buttons=( 
                       [Button.url('✅Məni Qrupa Sal✅', 'http://t.me/LedyTagRobot?startgroup=botstart')],
